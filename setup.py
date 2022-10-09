a2789d85316ef7199198623adc7208a1194b9783
henrihapponen <henkka.happonen@gmail.com>
2022-10-09 13:38:51 +0100

    Project setup changes

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Operating System :: Microsoft :: Windows :: Windows 10',
-    'Operating System :: MacOS :: MacOS X',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python'
-]
-
-setup(
-    name='docxedit',
-    version='0.0.1',
-    description='Edit Word documents, keep original format.',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='https://github.com/henrihapponen/docxedit',
-    download_url='https://github.com/henrihapponen/docxedit/archive/refs/tags/0.0.1.tar.gz',
-    author='Henri Happonen',
-    author_email='henkka.happonen@gmail.com',
-    license='MIT',
-    classifiers=classifiers,
-    keywords=['docx', 'python-docx', 'docxedit'],
-    packages=find_packages,
-    install_requires=['python-docx>=0.8.0']
-)
+if __name__ == "__main__":
+    setuptools.setup()
